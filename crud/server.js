6f194adb1df766f308849a37d7fe541ba011b216
Mateus Martins <mateusmartins@Mac-mini-de-Mateus.local>
2017-09-21 10:08:26 -0300

    acesso

--- crud/server.js
+++ crud/server.js
@@ -15,6 +15,12 @@ app.use(session({
 var crypto = require("crypto")
 var key = "supersecretkey";
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.static('/site/views'));
 
 
--- site/server.js
+++ site/server.js
@@ -10,6 +10,12 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 router.use(function (req, res, next) {
     console.log("/" + req.method);
